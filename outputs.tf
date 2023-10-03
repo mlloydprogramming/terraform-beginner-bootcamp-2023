91952cff9be219ebfcf6c6e1d41fe243aac07f95
Mathew Lloyd <mlloyd@boomtownroi.com>
2023-10-03 03:55:32 +0000

    #23 Changed bucket functionality to no longer use random provider

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "random_bucket_name_result" {
-    value = random_string.bucket_name.result
+output "name_result" {
+    value = aws_s3_bucket.website_bucket.bucket
 }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,13 @@ variable "user_uuid" {
     error_message    = "The user_uuid value is not a valid UUID."
   }
 }
+
+variable "bucket_name" {
+  description = "AWS S3 bucket name"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]{3,63}$", var.bucket_name))
+    error_message = "Bucket name must be between 3 and 63 characters and contain only lowercase letters, numbers, and hyphens."
+  }
+}
