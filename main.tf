d3bb8f9c1071dabcd16884435a060a6ca1e90701
Mathew Lloyd <mlloyd@boomtownroi.com>
2023-10-04 23:04:47 -0400

    #25 refactor s3 bucket into the new module structure (#26)

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
-  
-  tags = {
-    UserUuid        = var.user_uuid
-  }
+terraform {
+#   cloud {
+#     organization = "Mlloydprojects"
+
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
+}
+
+module "terrahouse_aws" {
+  source = "./module/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
