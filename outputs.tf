7eeca948412143317986e3ca25050910a50255e1
Mathew Lloyd <mlloyd@boomtownroi.com>
2023-10-12 02:39:33 +0000

    should be able to run terraform provider block without errors

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for out static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for out static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  description = "S3 Static website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "S3 Static website hosting endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "URL for S3 cloudfront distribution domain name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "URL for S3 cloudfront distribution domain name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
