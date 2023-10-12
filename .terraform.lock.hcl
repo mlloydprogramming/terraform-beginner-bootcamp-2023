44fe3c8676749d3e55e88512669b9a649f95128c
Mathew Lloyd <mlloyd@boomtownroi.com>
2023-10-12 03:30:17 +0000

    setup Skeleton for resource

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:/P6Na53ix1cgaEguKYe/iGZFWxVdfOBtKd+HKvMVCIo=",
+    "h1:oATOhKzZQBe6MgoNg0uZmZjSQP4XBFiEGTVu27HC5DM=",
   ]
 }
--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -1,11 +1,15 @@
 package main
+
 // package main: declares the package name. The main package is special in Go, it's where the execution of the program starts.
 
-
 import (
-	// "log"
+	"context"
+	"fmt"
+	"log"
+
 	// "fmt"
-	// "github.com/google/uuid"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
@@ -13,51 +17,101 @@ import (
 // import "fmt": Imports the declared packages, which contains functions for formatted I/O.
 
 // func main(): Defines the main function, the entry point of the application. When you run the program, it starts executing from this function.
-func main () {
+func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: Provider,
 	})
 }
 
+type Config struct {
+	Endpoint string
+	Token string
+	UserUuid string
+}
+
 // in golang, a titlecase function will get exported
 func Provider() *schema.Provider {
 	var p *schema.Provider
-	p = &schema.Provider {
-		ResourcesMap: map[string]*schema.Resource{
-
+	p = &schema.Provider{
+		ResourcesMap:   map[string]*schema.Resource{
+			"terratowns_home": Resource(),
 		},
-		DataSourcesMap: map[string]*schema.Resource{
-
-		},
+		DataSourcesMap: map[string]*schema.Resource{},
 		Schema: map[string]*schema.Schema{
 			"endpoint": {
-				Type: schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
 				Description: "The endpoint for the external service",
 			},
 			"token": {
-				Type: schema.TypeString,
-				Sensitive: true, // make the token as sensitive to hide it in the logs.
-				Required: true,
+				Type:        schema.TypeString,
+				Sensitive:   true, // make the token as sensitive to hide it in the logs.
+				Required:    true,
 				Description: "Bearer token for authorization",
 			},
 			"user_uuid": {
-				Type: schema.TypeString,
-				Required: true,
-				Description: "UUID for configuration",
-				// ValidateFunc: validateUUID
+				Type:         schema.TypeString,
+				Required:     true,
+				Description:  "UUID for configuration",
+				ValidateFunc: validateUUID,
 			},
 		},
 	}
-	// p.ConfigureContextFunc = providerConfigure(p)
+	p.ConfigureContextFunc = providerConfigure(p)
 	return p
 }
 
-// func validateUUID(val interface{}, k string) (ws []string, errors []error) {
-// 	log.Print('validateUUID:start')
-// 	value := v.(string)
-// 	if _,err = uuid.Parse(value); err !=nil {
-// 		errors = append(error, fmt.Errorf("invalid UUID format"))
-// 	}
-// 	log.Print(`validateUUID:end`)
-// }
+func validateUUID(v interface{}, k string) (ws []string, errors []error) {
+	log.Print("validateUUID:start")
+	value := v.(string)
+	if _, err := uuid.Parse(value); err != nil {
+		errors = append(errors, fmt.Errorf("invalid UUID format"))
+	}
+	log.Print(`validateUUID:end`)
+	return
+}
+
+func providerConfigure(p *schema.Provider) schema.ConfigureContextFunc {
+	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+		log.Print("providerConfigure:start")
+		config := Config {
+			Endpoint: d.Get("endpoint").(string),
+			Token: d.Get("endpoint").(string),
+			UserUuid: d.Get("user_uuid").(string),
+		}
+		log.Print("providerConfigure:end")
+		return &config, nil
+	}
+}
+
+func Resource() *schema.Resource {
+	log.Print("resource:start")
+	resource := &schema.Resource {
+		CreateContext: resourceHouseCreate,
+		ReadContext: resourceHouseRead,
+		UpdateContext: resourceHouseUpdate,
+		DeleteContext: resourceHouseDelete,
+	}
+	log.Print("resource:end")
+	return resource
+}
+
+func resourceHouseCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceHouseRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceHouseUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceHouseDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
